7d393d65707a2b6e832d9bf623b93cfaff716145
RobertW85 <22125rw@hvhs.school.nz>
2026-02-04 12:04:07 +1300

    JavaFinished

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 var messageSpace = document.getElementById("welcomeMessage");
 messageSpace.innerHTML = "You've connected to the JavaScript!";
+
+function buttonFunc(num){
+    if (num == 1){
+        messageSpace.innerHTML = "You pressed the button!"
+    }else{
+        messageSpace.innerHTML = document.getElementById("input").value
+    }
+}
